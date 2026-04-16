841a85bacba6a7b9d5d14e575818a7d4d07dbfcf
Mohammed Mokeddem <mohammed.mokeddem69@gmail.com>
2026-04-16 15:46:36 +0000

    clean and enrich staging trips model

--- models/staging/stg_trips.sql
+++ models/staging/stg_trips.sql
@@ -1,1 +1,21 @@
-select 1 as test
+select
+
+    vendor_id,
+    pickup_datetime as pickup_at,
+    dropoff_datetime as dropoff_at,
+    passenger_count,
+    trip_distance,
+    total_amount,
+    date(pickup_datetime) as trip_date,
+    timestamp_diff(
+        dropoff_datetime,
+        pickup_datetime,
+        minute
+    ) as trip_duration_min
+
+from {{ source('taxi', 'tlc_yellow_trips_2022') }}
+
+where trip_distance > 0
+  and total_amount > 0
+
+limit 1000
